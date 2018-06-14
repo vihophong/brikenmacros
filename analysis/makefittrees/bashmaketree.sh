86d5033a89d8c46c407df000764f02c787e7d0df
vihophong <hophongmc@gmail.com>
2018-06-14 13:02:00 +0900

    new
    Former-commit-id: 67939002e63c979d850c03345c524c7eda5e9a30

---
+++ analysis/makefittrees/bashmaketree.sh
@@ -0,0 +1,6 @@
+./offlinemakemlhtree.sh list/listlowin.txt In131 /home/phong/briken17/results/fithistograms/In131lowin.root
+./offlinemakemlhtree.sh list/listlowin.txt In132 /home/phong/briken17/results/fithistograms/In132lowin.root
+./offlinemakemlhtree.sh list/listlowin.txt In133 /home/phong/briken17/results/fithistograms/In133lowin.root
+./offlinemakemlhtree.sh list/listlowin.txt In134 /home/phong/briken17/results/fithistograms/In134lowin.root
+./offlinemakemlhtree.sh list/listlowin.txt In135 /home/phong/briken17/results/fithistograms/In135lowin.root
+./offlinemakemlhtree.sh list/listlowin.txt In136 /home/phong/briken17/results/fithistograms/In136lowin.root

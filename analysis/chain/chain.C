06095e53d3712ca29d0767eaf2826f675fbd8b32
vihophong <hophongmc@gmail.com>
2018-11-30 18:26:52 +0900

    asofNov30
    Former-commit-id: ba598d285c092da7c72dc352d634c3ea759a0377 [formerly ae739670f5ae622091753a9d192f4da32c763c96]
    Former-commit-id: 373aad64f1d0b396f59d320ff65f2d2cecac5f03

--- analysis/chain/chain.C
+++ analysis/chain/chain.C
@@ -55,6 +55,32 @@ void chaineachimplant(char* listfile,char* pid){
       ch->Add(tempchar2);
   }
 }
+
+void chainallneutron(char* listfile){
+  char pid[500];
+  sprintf(pid,"neutron");
+  char tempchar1[1000];
+  sprintf(tempchar1,"tree%s",pid);
+  TChain* ch = new TChain(tempchar1);
+  std::ifstream ifs(listfile);
+  string filelist[1000];
+
+  Int_t nfiles=0;
+  while (!ifs.eof()){
+      ifs>>filelist[nfiles];
+      cout<<filelist[nfiles]<<endl;
+      nfiles++;
+  }
+  nfiles=nfiles-1;
+  cout<<"There are "<<nfiles<<" files in total!"<<endl;
+
+  for (Int_t i=0;i<nfiles;i++){
+      char tempchar2[1000];
+      sprintf(tempchar2,"%s/tree%s",filelist[i].c_str(),pid);
+      ch->Add(tempchar2);
+  }
+}
+
 void chainimplant(char* listfile,char* pidfile){
     std::ifstream ifspid(pidfile);
     Int_t nri=0;

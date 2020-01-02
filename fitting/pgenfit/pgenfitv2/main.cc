2bcc86d7a6903f381f85da55892742d6be3918f4
vihophonghus <vihophong@hus.edu.vn>
2020-01-02 16:38:47 +0700

    new program for MLH fitting at fitting/pgenfit/pgenfitv2

--- fitting/pgenfit/pgenfitv2/main.cc
+++ fitting/pgenfit/pgenfitv2/main.cc
@@ -3,8 +3,9 @@
 
 int main(int argc, char *argv[])
 {
+    unbinfit* fit;
     if (argc==2) {
-        unbinfit* fit=new unbinfit;
+        fit=new unbinfit;
         char inpparms[1000];
         char outpparms[1000];
         sprintf(inpparms,"parmsex.txt");
@@ -12,11 +13,13 @@ int main(int argc, char *argv[])
         fit->Init(inpparms,outpparms);
         fit->Run(argv[1]);
     }else if(argc==4){
-        unbinfit* fit=new unbinfit;
+        fit=new unbinfit;
         fit->Init(argv[1],argv[2]);
         fit->Run(argv[3]);
     }else{
         std::cout<<"check inputs!"<<std::endl;
         return 0;
     }
+    delete fit;
+    return 0;
 }
